01c51adac059c545d826ca09dbfd30c1bd43fef1
darren <macbook@darren.local>
2025-07-16 19:19:36 +0800

    update auth.ts

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -2,8 +2,6 @@ import NextAuth from "next-auth"
 import Nodemailer from "next-auth/providers/nodemailer"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-
-<<<<<<< HEAD
 const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
