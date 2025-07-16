6d36f8e180b101007aa7206d3a2044001437d018
darren <macbook@darren.local>
2025-07-16 19:21:21 +0800

    update auth.ts

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -2,29 +2,6 @@ import NextAuth from "next-auth"
 import Nodemailer from "next-auth/providers/nodemailer"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient();
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    trustHost: true,
-    providers: [
-        Nodemailer({
-            server: {
-                host: "smtp.gmail.com",
-                port: 587,
-                secure: false, // true for 465, false for other ports
-                auth: {
-                    user: process.env.GMAIL_USER, // your-email@gmail.com
-                    pass: process.env.GMAIL_APP_PASSWORD, // your Gmail app password
-                },
-            },
-            from: process.env.GMAIL_USER,
-        }),
-    ],
-    pages: {
-        verifyRequest: "/verify"
-    }
-})
 const prisma = new PrismaClient()
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
